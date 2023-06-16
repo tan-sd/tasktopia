a90ccdd458bfa39eb28e7e6a64f916d9fe950cb4
tan-sd <shengdatan@gmail.com>
2023-06-16 14:31:07 +0800

    Replace component

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import InkFish from './assets/components/InkFish';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import AnimalSelection from './assets/components/AnimalSelection';
 
 export default function App() {
   return (
     <>
-    <View style={styles.container}>
+    <View style={styles.header}>
       <Text>Choose your pets</Text>
       <StatusBar style="auto" />
     </View>
-    <InkFish />
+    <AnimalSelection/>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    width: '100%',
+    height: 100,
+    textAlign: 'center',
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
