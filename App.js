6e6c1991abb68e67acec912ed8f0ed6fefdf3bf0
Sheng <shengdatan@gmail.com>
2023-06-17 11:16:49 +0800

    Add Stack and Tab navigator

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import AnimalSelection from './components/AnimalSelection';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginPage from './screens/LoginPage';
+import AnimalSelection from './screens/AnimalSelection';
+import HomePage from './screens/HomePage';
+import RewardsPage from './screens/RewardsPage';
+import ProfilePage from './screens/ProfilePage';
+import FriendsPage from './screens/FriendsPage';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(
+  ['Scripts "build/three.js" and "build/three.min.js" are deprecated with r150+, and will be removed with r160. Please use ES Modules or alternatives: https://threejs.org/docs/index.html#manual/en/introduction/Installation']
+  )
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomePage} options={{ headerShown: false }}/>
+      <Tab.Screen name="Rewards" component={RewardsPage} options={{ headerShown: false }} />
+      <Tab.Screen name="Profile" component={ProfilePage} options={{ headerShown: false }} />
+      <Tab.Screen name="Friends" component={FriendsPage} options={{ headerShown: false }} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <>
-    <View style={styles.header}>
-      <Text>Choose your pets</Text>
-      <StatusBar style="auto" />
-    </View>
-    <AnimalSelection/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginPage}
+          options={{ headerShown: false }}
+          // options={{title: 'Welcome'}}
+          />
+
+        <Stack.Screen
+          name="AnimalSelection"
+          component={AnimalSelection}
+          />
+
+        <Stack.Screen
+          name="HomePage"
+          component={Home}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    width: '100%',
-    height: 100,
-    textAlign: 'center',
-    marginTop: 50,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
