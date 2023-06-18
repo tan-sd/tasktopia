914dd9b47e166bfb89733483990a61c5340ca881
rrachea <rrachelsng@gmail.com>
2023-06-18 18:53:53 +0800

    style login

--- screens/LoginPage.jsx
+++ screens/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput, SafeAreaView, TouchableHighlight } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 
@@ -12,22 +12,32 @@ export default function LoginPage({navigation}) {
             <StatusBar/>
             <Text
                 style={styles.homeHeader}
-            >Tasktopia</Text>
+            >TASK</Text>
+            <Text
+                style={styles.homeHeaderSecond}
+            >TOPIA</Text>
 
-            <Text>Username</Text>
+            <View style={styles.middleSection}>
+            <Text style={styles.labels}>Username</Text>
             <TextInput 
                 style={styles.textInput}
                 onChangeText={onChangeUsername}
             />
-            <Text>Password</Text>
+            <Text style={styles.labels}>Password</Text>
             <TextInput 
                 style={styles.textInput}
                 secureTextEntry={true}
                 onChangeText={onChangePassword}
                 />
-            <Button
+            </View> 
+                {/* <TouchableHighlight style={styles.button}>
+            <Button 
                 title="Login"
-                onPress={() => navigation.navigate('AnimalSelection')}></Button>
+                onPress={() => navigation.navigate('AnimalSelection')}/></TouchableHighlight> */}
+            <View style={styles.login}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AnimalSelection')}>
+                <Text style={{textAlign: 'center', marginTop: 10, color:'white'}}>Login</Text>
+            </TouchableOpacity></View>
         </SafeAreaView>
         </>
     )
@@ -39,8 +49,38 @@ const styles = StyleSheet.create({
         margin: 12,
         borderWidth: 1,
         padding: 10,
+        width:196,
+        borderRadius:5,
     },
     homeHeader: {
+        textAlign: 'center',
+        color: '#EF524A',
+    },
+    homeHeaderSecond: {
         textAlign: 'center'
+    },
+    labels:{
+        color: '#EF524A',
+    },
+    middleSection:{
+        display: 'flex',
+        alignItems: 'center',
+        top: 300,
+    },
+    button:{
+        height:40,
+        width:160,
+        backgroundColor: "#16D7E1",
+        borderRadius: 20,
+        top:330,
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    login:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
