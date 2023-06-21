5608e99b4f76076c5e34a97a23dad28444f2212a
rrachea <rrachelsng@gmail.com>
2023-06-21 22:16:37 +0800

    admin pg

--- screens/AdminPage.jsx
+++ screens/AdminPage.jsx
@@ -1,127 +1,94 @@
-import React from 'react';
 import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput, SafeAreaView, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { useFonts } from 'expo-font';
+import { auth } from "../firebase/firebase";
 import {ref as ref_database, onValue} from 'firebase/database';
 import { database } from "../firebase/firebase";
 import { getStorage, ref as ref_storage, getDownloadURL } from '@firebase/storage';
 
-export default function AdminPage(){
+export default function workersPage({navigation}) {
+    const [workers, setWorkers] = React.useState(null);
+    const [profileP, setProfileP] = React.useState('');
+    const [convertedData, setConvertedData] = React.useState([]);
+    const storage = getStorage();
 
+    const handleVisitPress = (userDetails) => {
+        navigation.navigate('UserTasks', { userDetails });
+      };  
+
+    React.useEffect(() => {
+        const fetchDataAndConvertImages = async () => {
+          try {
+            const workersRef = ref_database(database, 'users/');
+            onValue(workersRef, (snapshot) => {
+              const data = snapshot.val();
+    
+              if (data) {
+                convertAndSetData(data);
+              }
+            });
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchDataAndConvertImages();
+      }, []);
+    
+      const convertAndSetData = async (data) => {
+        const convertedData = [];
+    
+        for (const userId in data) {
+          const user = data[userId];
+          const { selectedPet, firstName, lastName, jobRole, profileImg } = user;
+    
+          if (profileImg) {
+            try {
+              const storageRef = ref_storage(storage, profileImg);
+              const url = await getDownloadURL(storageRef);
+    
+              const convertedUser = {
+                selectedPet,
+                userId,
+                firstName,
+                lastName,
+                jobRole,
+                profileImg: url,
+              };
+    
+              convertedData.push(convertedUser);
+            } catch (error) {
+              console.error('Error converting image URL:', error);
+            }
+          }
+        }
+    
+        setConvertedData(convertedData);
+      };
+        
     const [loaded] = useFonts({
         GothamBold: require('../assets/fonts/Gotham-Bold.otf'),
         GothamBook: require('../assets/fonts/Gotham-Book.otf')
       });
 
-
-
-      const [worker, setWorker] = React.useState('');
-      const [task, setTask] = React.useState('');
-      const [isChecked, setIsChecked] = React.useState(false);
-      
-    //   get friends data with image
-      const [friends, setFriends] = React.useState(null);
-      const [profileP, setProfileP] = React.useState('');
-      const [convertedData, setConvertedData] = React.useState([]);
-      const storage = getStorage();
-
-
-      var data = '';
-    const friendsRef = ref_database(database, 'users/');
-
-    React.useEffect(()=>{
-        onValue(friendsRef, (snapshot) => {
-            data = snapshot.val();
-        })
-        setFriends(data);
-    }, [])
-
-    
-
-    const convertImageURLs = async (data) => {
-        const convertedData = [];
-
-        for (const userId in data) {
-            const user = data[userId];
-            const { firstName, lastName, jobRole, profileImg } = user;
-
-            if (profileImg) {
-                try {
-                    const storage = getStorage();
-                    const storageRef = ref_storage(storage, profileImg);
-                    const url = await getDownloadURL(storageRef);
-
-                    const convertedUser = {
-                    userId,
-                    firstName,
-                    lastName,
-                    jobRole,
-                    profileImg: url,
-                    };
-
-                    convertedData.push(convertedUser);
-                } catch (error) {
-                    console.error('Error converting image URL:', error);
-                }
-            }
-        }
-    return convertedData;
-    };
-
-    const handleDataConversion = async () => {
-    try {
-        const convertedData = await convertImageURLs(data);
-        // Use the converted data in your React Native component or perform further operations
-        console.log(convertedData);
-    } catch (error) {
-        console.error('Error converting image URLs:', error);
-    }
-    };
-
-    handleDataConversion();
-
-    React.useEffect(() => {
-        const fetchDataAndConvertImages = async () => {
-
-        const convertedData = await convertImageURLs(data);
-        
-        setConvertedData(convertedData);
-        };
-        
-        fetchDataAndConvertImages();
-    }, []);
-
-    //   const setWorkerName = (text) => {
-    //     setWorker(text);
-    //   };
-    
-    //   const handleInputChange2 = (text) => {
-    //     setTask(text);
-    //   };
-    
-    //   const handleCheckBoxChange = () => {
-    //     setIsChecked(!isChecked);
-    //   };
-    
-    //   const handleButtonPress = () => {
-    //     // Handle button press logic
-    //   };
-    
       if (!loaded) {
         return null;
       }
-    return(
+
+    return (
         <>
         <SafeAreaView style={styles.container}>
             <ScrollView>
             <StatusBar/>
-            <View style={styles.adminWrapper}>
+            <View style={styles.workersWrapper}>
                 <View>
-                    <Text style={styles.adminHeader}>Admin</Text>
+                    <Text
+                        style={[styles.workersHeader, styles.gothamBold]}
+                    >My workers</Text>
                 </View>
-            </View>
 
-            <View>
+                <View>
                     { convertedData ? convertedData.map((item) => {
                         return( 
                             <>
@@ -129,15 +96,18 @@ export default function AdminPage(){
                             <View>
                             <Image
                                 source={{uri: item.profileImg}}
-                                style={styles.friendsImg}
+                                style={styles.workersImg}
                             />
                             </View>
 
                             <View style={styles.friendDetailsWrapper}>
                                 <Text style={[styles.friendName, styles.gothamBold]}>{item.firstName} {item.lastName}</Text>
                                 <Text style={[styles.friendJobRole, styles.gothamBook]}>{item.jobRole}</Text>
-                                <TouchableOpacity style={styles.visitButton}>
-                                    <Text style={{...styles.gothamBook, textAlign: 'center'}}>Manage User Tasks</Text>
+                                <TouchableOpacity
+                                    style={styles.visitButton}
+                                    onPress={() => handleVisitPress(item)}
+                                    >
+                                    <Text style={{...styles.gothamBook, textAlign: 'center'}}>Manage User</Text>
                                 </TouchableOpacity>
                             </View>
                             </View>
@@ -148,8 +118,7 @@ export default function AdminPage(){
                         )
                     }
                     </View>
-
-   
+                </View>
             </ScrollView>
         </SafeAreaView>
         </>
@@ -161,27 +130,14 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fedb7d',
     },
-    adminHeader:{
-        fontSize: 35,
-    },
-    adminWrapper: {
+    workersWrapper: {
         marginLeft: 40,
         marginTop: 15
     },
-    gothamBold: {
-        fontFamily: 'GothamBold'
-    },
-    gothamBook: {
-        fontFamily: 'GothamBook'
-    },
-    friendsWrapper: {
-        marginLeft: 40,
-        marginTop: 15
-    },
-    friendsHeader: {
+    workersHeader: {
         fontSize: 35,
     },
-    friendsImg: {
+    workersImg: {
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 50,
@@ -206,5 +162,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         width: 175,
         marginTop: 15
+    },
+    gothamBold: {
+        fontFamily: 'GothamBold'
+    },
+    gothamBook: {
+        fontFamily: 'GothamBook'
     }
 })
